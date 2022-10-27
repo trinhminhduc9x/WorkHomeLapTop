7ce1cae3bccb1adbb7fac80fce343df110d10e89
trinhminhduc9x <duc1997th@gmail.com>
2022-10-27 19:03:25 +0700

    add

---
+++ product/src/main/java/product/service/IProductService.java
@@ -0,0 +1,10 @@
+package product.service;
+
+import product.model.Product;
+
+import java.util.Optional;
+
+public interface IProductService {
+    Iterable<Product> findAll();
+    Optional<Product> findById(Long id);
+}
