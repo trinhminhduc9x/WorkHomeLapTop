695a2c3eafb864a0b174480fe953191bf0fb4bf0
trinhminhduc9x <duc1997th@gmail.com>
2022-11-09 00:46:50 +0700

    Add

--- CG-DN Case Study for Spring_v3.0/CaseStudyFurama/src/main/java/casestudy/repository/contract/IContractRepository.java
+++
@@ -1,9 +0,0 @@
-package casestudy.repository.contract;
-
-import casestudy.model.contract.Contract;
-import casestudy.model.customer.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IContractRepository extends JpaRepository<Contract,Integer> {
-
-}
