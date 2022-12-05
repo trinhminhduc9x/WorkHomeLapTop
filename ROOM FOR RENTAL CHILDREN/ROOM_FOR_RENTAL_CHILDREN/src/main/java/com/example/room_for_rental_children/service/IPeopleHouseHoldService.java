3b7840b362a5a3ea52493dd37239e3cb407e128a
trinhminhduc9x <duc1997th@gmail.com>
2022-12-05 18:18:54 +0700

    add

---
+++ ROOM FOR RENTAL CHILDREN/ROOM_FOR_RENTAL_CHILDREN/src/main/java/com/example/room_for_rental_children/service/IPeopleHouseHoldService.java
@@ -0,0 +1,10 @@
+package com.example.room_for_rental_children.service;
+
+
+import com.example.room_for_rental_children.model.PeopleHouseHold;
+
+import java.util.List;
+
+public interface IPeopleHouseHoldService {
+    List<PeopleHouseHold> FindListAll();
+}
