03b05078bc46570175492212c50d1fea62860361
trinhminhduc9x <duc1997th@gmail.com>
2023-01-30 11:40:49 +0700

    Add

--- BE/project-backend/src/main/java/projectbackend/controller/CustomerRestController.java
+++ BE/project-backend/src/main/java/projectbackend/controller/CustomerRestController.java
@@ -5,11 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import projectbackend.dto.customer.CustomerDto;
-
 
 import projectbackend.model.customer.Customer;
 import projectbackend.service.customer.ICustomerService;
@@ -28,18 +27,18 @@ public class CustomerRestController {
     @Autowired
     private ICustomerService iCustomerService;
 
-    @PostMapping("/add")
-    public ResponseEntity<?> saveCustomer(@RequestBody @Valid CustomerDto customerDto, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return new ResponseEntity<>(bindingResult.getFieldErrors(),
-                    HttpStatus.BAD_REQUEST);
-        }
-        Customer customer = new Customer();
-        BeanUtils.copyProperties(customerDto, customer);
-        customer.getAccount().setPassword(new BCryptPasswordEncoder().encode(customerDto.getAccount().getPassword()));
-        iCustomerService.saveCustomerByUser(customer);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
+//    @PostMapping("/add")
+//    public ResponseEntity<?> saveCustomer(@RequestBody @Valid CustomerDto customerDto, BindingResult bindingResult) {
+//        if (bindingResult.hasErrors()) {
+//            return new ResponseEntity<>(bindingResult.getFieldErrors(),
+//                    HttpStatus.BAD_REQUEST);
+//        }
+//        Customer customer = new Customer();
+//        BeanUtils.copyProperties(customerDto, customer);
+//        customer.getAccount().setPassword(new BCryptPasswordEncoder().encode(customerDto.getAccount().getPassword()));
+//        iCustomerService.saveCustomerByUser(customer);
+//        return new ResponseEntity<>(HttpStatus.OK);
+//    }
 
     @GetMapping("")
     public ResponseEntity<List<Customer>> showList() {
--- BE/project-backend/src/main/java/projectbackend/service/customer/impl/CustomerService.java
+++ BE/project-backend/src/main/java/projectbackend/service/customer/impl/CustomerService.java
@@ -3,8 +3,8 @@ package projectbackend.service.customer.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import projectbackend.dto.customer.ICustomerDto;
 import projectbackend.dto.customer.ICustomerStatementDto;
@@ -22,17 +22,17 @@ public class CustomerService implements ICustomerService {
     @Autowired
     private ICustomerRepository customerRepository;
 
-    @Override
-    public void saveCustomer(Customer customer) {
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        customerRepository.setPassword(customer.getAccount().getAccountname(),
-                passwordEncoder.encode(customer.getAccount().getPassword()));
-        customerRepository.save(customer);
-    }
+//    @Override
+//    public void saveCustomer(Customer customer) {
+//        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+//        customerRepository.setPassword(customer.getAccount().getAccountname(),
+//                passwordEncoder.encode(customer.getAccount().getPassword()));
+//        customerRepository.save(customer);
+//    }
 
     @Override
     public List<Customer> findListAll() {
-        return null;
+        return customerRepository.findAll();
     }
 
 
@@ -41,7 +41,10 @@ public class CustomerService implements ICustomerService {
         customerRepository.saveCustomer(customer);
     }
 
+    @Override
+    public void saveCustomer(Customer customer) {
 
+    }
 
 
 }
